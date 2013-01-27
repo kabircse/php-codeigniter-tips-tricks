b83c5f293117f307fb36877fef5928e9ec369b4e
Sonny Nguyen <phplaw@gmail.com>
2013-01-27 11:32:30 -0800

    Create ci_my_output_class.php

---
+++ ci_my_output_class.php
@@ -0,0 +1,19 @@
+Create MY_Output.php in “application/libraries” containing the following code…
+
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Output extends CI_Output {
+
+    function nocache()
+    {
+        $this->set_header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+        $this->set_header('Cache-Control: no-cache, no-store, must-revalidate, max-age=0');
+        $this->set_header('Cache-Control: post-check=0, pre-check=0', FALSE);
+        $this->set_header('Pragma: no-cache');
+    }
+
+}
+// USAGE
+//$this->output->nocache();
+
+/* End of File */
