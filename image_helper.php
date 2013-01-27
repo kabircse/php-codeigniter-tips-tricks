aaeabb8e3d463a5c9e1f834d8a8b6b4c5d41ea5f
Sonny Nguyen <phplaw@gmail.com>
2013-01-27 11:21:58 -0800

    Create image_helper.php
    Add Image cache

---
+++ image_helper.php
@@ -0,0 +1,32 @@
+function image_thumb($image_path, $height, $width)
+{
+    // Get the CodeIgniter super object
+    $CI =& get_instance();
+
+    // Path to image thumbnail
+    $image_thumb = dirname($image_path) . '/' . $height . '_' . $width . '.jpg';
+
+    if( ! file_exists($image_thumb))
+    {
+        // LOAD LIBRARY
+        $CI->load->library('image_lib');
+
+        // CONFIGURE IMAGE LIBRARY
+        $config['image_library']    = 'gd2';
+        $config['source_image']     = $image_path;
+        $config['new_image']        = $image_thumb;
+        $config['maintain_ratio']   = TRUE;
+        $config['height']           = $height;
+        $config['width']            = $width;
+        $CI->image_lib->initialize($config);
+        $CI->image_lib->resize();
+        $CI->image_lib->clear();
+    }
+
+    return '<img src="' . dirname($_SERVER['SCRIPT_NAME']) . '/' . $image_thumb . '" />';
+}
+// USAGE
+/* echo image_thumb('assets/images/picture-1/picture-1.jpg', 50, 50); */
+
+/* End of file image_helper.php */
+/* Location: ./application/helpers/image_helper.php */
