242dc6c30a6004ad66c5be77c655213928a3f34a
Sonny <phplaw@gmail.com>
2013-07-30 16:53:29 +0700

    rearrange source code with category directory

---
+++ javascript/fullscreen_api.js
@@ -0,0 +1,14 @@
+// Find the right method, call on correct element
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+
+// Launch fullscreen for browsers that support it!
+launchFullScreen(document.documentElement); // the whole page
+launchFullScreen(document.getElementById("videoElement")); // any individual element
