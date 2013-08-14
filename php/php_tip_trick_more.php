50c9622a89624c85beeee573b09a168a834fb2bb
Sonny Nguyen <phplaw@gmail.com>
2013-08-14 17:54:12 +0700

    Update php_tip_trick_more.php
    Add https check

--- php/php_tip_trick_more.php
+++ php/php_tip_trick_more.php
@@ -107,3 +107,20 @@ $filename = substr($file_full_path, strrpos($file_full_path, '/') + 1);
 $filename_without_extension = substr($filename, 0, strrpos($filename, '.'));
 // get file extension
 $fileext = substr($filename, strrpos($filename, '.') + 1);
+
+// check http has ssl
+$http = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
+
+// from wordpress/wp-includes/functions.php
+function is_ssl() {
+   if ( isset($_SERVER['HTTPS']) ) {
+       if ( 'on' == strtolower($_SERVER['HTTPS']) )
+           return true;
+       if ( '1' == $_SERVER['HTTPS'] )
+           return true;
+   } elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' ==
+$_SERVER['SERVER_PORT'] ) ) {
+       return true;
+   }
+   return false;
+}
