037d1774d23a6f70b2cfe45bfffb38524a8abdfa
Sonny Nguyen <phplaw@gmail.com>
2013-08-22 16:36:26 +0700

    Update MY_Input.php
    Add input stream

--- codeigniter/MY_Input.php
+++ codeigniter/MY_Input.php
@@ -1,4 +1,5 @@
 <?php
+
 class MY_Input extends CI_Input {
 
     var $delete;
@@ -24,6 +25,32 @@ class MY_Input extends CI_Input {
 
     function put($index = '', $xss_clean = FALSE) {
         return $this->_fetch_from_array($this->put, $index, $xss_clean);
+    }
+
+    /**
+     * Fetch an item from the php://input stream
+     *
+     * Useful when you need to access PUT, DELETE or PATCH request data.
+     *
+     * @param string $index Index for item to be fetched
+     * @param bool $xss_clean Whether to apply XSS filtering
+     * @return mixed
+     */
+    public function input_stream($index = '', $xss_clean = FALSE) {
+// The input stream can only be read once, so we'll need to check
+// if we have already done that first.
+        if (is_array($this->_input_stream)) {
+            return $this->_fetch_from_array($this->_input_stream, $index, $xss_clean);
+        }
+
+// Parse the input stream in our cache var
+        parse_str(file_get_contents('php://input'), $this->_input_stream);
+        if (!is_array($this->_input_stream)) {
+            $this->_input_stream = array();
+            return NULL;
+        }
+
+        return $this->_fetch_from_array($this->_input_stream, $index, $xss_clean);
     }
 
 }
