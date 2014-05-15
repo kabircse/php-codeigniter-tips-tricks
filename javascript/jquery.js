a4aac2ad3c7dbe5374db51e51dd2db55736227f0
Sonny Nguyen <phplaw@gmail.com>
2014-05-15 16:41:02 +0700

    Update jquery.js

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,1 +1,15 @@
+// trim, rtrim, ltrim
+function trim(str, chr) {
+  var rgxtrim = (!chr) ? new RegExp('^\\s+|\\s+$', 'g') : new RegExp('^'+chr+'+|'+chr+'+$', 'g');
+  return str.replace(rgxtrim, '');
+}
+function rtrim(str, chr) {
+  var rgxtrim = (!chr) ? new RegExp('\\s+$') : new RegExp(chr+'+$');
+  return str.replace(rgxtrim, '');
+}
+function ltrim(str, chr) {
+  var rgxtrim = (!chr) ? new RegExp('^\\s+') : new RegExp('^'+chr+'+');
+  return str.replace(rgxtrim, '');
+}
+
 jQuery('img[src*="?q="]').each(function(i,e) { alert(jQuery(e).attr('src')); });
