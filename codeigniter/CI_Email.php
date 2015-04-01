3e919fbd034617ed87141c2145db7aaada74e658
Kabir Hossain <kabir.pust@gmail.com>
2015-04-01 14:35:36 +0600

    Create CI_Email.php

---
+++ codeigniter/CI_Email.php
@@ -0,0 +1,25 @@
+<?php
+// Email configuration
+$config = Array(
+'protocol' => 'smtp',
+'smtp_host' => 'ssl://smtp.googlemail.com',
+'smtp_port' => 465,
+'smtp_user' => 'iftekharuddin51@gmail.com',
+'smtp_pass' => 'if0604052',
+'mailtype' => 'html',
+'charset' => 'iso-8859-1',
+'wordwrap' => TRUE
+);
+
+/* Email for live server configuration
+$config_live_server = Array(
+'protocol' => 'smtp',
+'smtp_host' => 'smtp.yourdomainname.com.',
+'smtp_port' => 465,
+'smtp_user' => 'admin@yourdomainname.com', // change it to yours
+'smtp_pass' => '******', // change it to yours
+'mailtype' => 'html',
+'charset' => 'iso-8859-1',
+'wordwrap' => TRUE
+);*/
+?>
