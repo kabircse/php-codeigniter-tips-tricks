8062fc34b927874d5d09186186947d2b7e1acc4b
Kabir Hossain <kabir.pust@gmail.com>
2015-08-19 10:54:58 +0600

    Update PHP functions.php

--- PHP functions.php
+++ PHP functions.php
@@ -8,3 +8,8 @@
 8. array_reverse($array)  //reverse array element; array(a=>2,b=>3,0=>4,d=>2) to array(d=>2,0=>4,b=>3,a=>2);
 9. array_search(4,$array)  //check an element is prsent to array; array(a=>2,b=>3,0=>4,d=>2) to present;
 10. array_unique($array)  // keeps unique values to array; array(a=>2,b=>3,0=>4,d=>2,e=>4) to array(a=>2,b=>3,0=>4);
+11. str_word_count("How many words do I have?")  //Outputs: 5
+12. levenshtein($str1, $str2); // Determine how different two words are;$str1 = "carrot"; $str2 = "carrrott";// Outputs:2;
+13. get_defined_vars() // Shows list of all defined variables used in the system.
+14. escapeshellcmd() // escape shell command like exec() or system();
+15. checkdate() // check a date is valid or invalid.
