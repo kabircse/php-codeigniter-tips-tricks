96561f6755cfac7314bb43897fe90ea4a8ef693b
Kabir Hossain <kabir.pust@gmail.com>
2016-01-14 17:12:02 +0600

    Update rough.php

--- rough.php
+++ rough.php
@@ -32,3 +32,34 @@ DELIMITER $$
 			END IF;
 			END;
 DELIMITER;
+
+
+
+/*Table structure for table `stock_in_out_by_date`
+--
+
+CREATE TABLE IF NOT EXISTS `stock_in_out_by_date` (
+  `id` bigint(20) NOT NULL,
+  `date` date NOT NULL,
+  `stock_item_id` int(11) NOT NULL,
+  `godown_id` int(11) NOT NULL,
+  `qtyi` float NOT NULL,
+  `ratei` float NOT NULL,
+  `totali` float NOT NULL,
+  `qtyo` float NOT NULL,
+  `rateo` float NOT NULL,
+  `totalo` float NOT NULL,
+  `color_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stock_in_out_by_date`
+--
+ALTER TABLE `stock_in_out_by_date`
+ ADD PRIMARY KEY (`id`);
+
+*/
